55497f5b475e5325dccad605b818d81659abbaf9
erickw00 <erik@renderheads.com>
2023-01-27 15:26:44 +0300

    @tesh254/kloner: disable ssh functionality

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"main/access"
 	"main/server"
 )
 
 func main() {
-	access.ConnectToServerWithPrivatePublicKeys("tora", "18.185.110.231", "22")
+	// access.ConnectToServerWithPrivatePublicKeys("tora", "18.185.110.231", "22")
 
 	server.StartServer()
 }
